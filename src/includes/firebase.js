b9483d5c21843e1347709fb70543bda4159a6e04
petrero <petrero@gmail.com>
2022-05-13 16:18:38 +0300

    Initial files

---
+++ src/includes/firebase.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0LUlB9icA44NwAsqhHgXVs_jfhbEJOqc",
+  authDomain: "music-7d294.firebaseapp.com",
+  projectId: "music-7d294",
+  storageBucket: "music-7d294.appspot.com",
+  appId: "1:884671287793:web:6672f715c2fb2a01eef450"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+const usersCollection = db.collection('users');
+const songsCollection = db.collection('songs');
+
+export {
+  auth,
+  db,
+  usersCollection,
+  songsCollection,
+  storage,
+};
