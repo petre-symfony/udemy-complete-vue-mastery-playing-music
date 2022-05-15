dd9882610dd1aec3e7c08bb9e9f1d73c5e21c787
petrero <petrero@gmail.com>
2022-05-15 09:01:27 +0300

    14.193 Prepping the form Provide feedback to the user to let them know the status of their submission.

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -22,13 +22,23 @@
 				<i class="fa fa-comments float-right text-green-400 text-2xl"></i>
 			</div>
 			<div class="p-6">
-				<vee-form :validation-schema="schema">
+				<div class="text-white text-center font-bold p-4 mb-4"
+						 v-if="comment_show_alert"
+						 :class="comment_alert_variant"
+				>
+					{{ comment_alert_message }}
+				</div>
+				<vee-form :validation-schema="schema" @submit="addComment">
           <vee-field as="textarea" name="comment"
 						class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
               duration-500 focus:outline-none focus:border-black rounded mb-4"
 						placeholder="Your comment here..."></vee-field>
 					<ErrorMessage class="text-red-600" name="comment"/>
-					<button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block">Submit</button>
+					<button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block"
+						:disabled="comment_in_submission"
+					>
+						Submit
+					</button>
 				</vee-form>
 
 				<select
@@ -126,7 +136,11 @@ export default {
 			song: {},
 			schema: {
 				comment: 'required|min:3'
-			}
+			},
+			comment_in_submission: false,
+			comment_show_alert: false,
+			comment_alert_variant: 'bg-blue-500',
+			comment_alert_message: 'Please wait! Your comment is being submitted'
 		}
 	},
 	async created() {
@@ -138,6 +152,14 @@ export default {
 		}
 
 		this.song = docSnapshot.data();
+	},
+	methods: {
+		async addComment(values) {
+			this.comment_in_submission = true;
+			this.comment_show_alert = true;
+			this.comment_alert_variant = 'bg-blue-500';
+			this.comment_alert_message = 'Please wait! Your comment is being submitted';
+		}
 	}
 }
 </script>
